1960a8aba327fc6eabcf50861926c71bfe438846
FrankXiong <1131076966@qq.com>
2016-04-28 03:07:12 +0800

    slide

--- src/js/runimg.js
+++ src/js/runimg.js
@@ -17,11 +17,23 @@
     window.onload = function(){
         var container = $('#runimg-container')[0],
             list = $('#runimg-list')[0],
-            buttons = $('#runimg-buttons .dot'),
+            buttons = $('#runimg-buttons span'),
             prev = $('#runimg-prev')[0],
             next = $('#runimg-next')[0],
+            index = 1,
             animated = false;
         
+        function showDot(){
+            for(var i=0;i<buttons.length;i++){
+                if(buttons[i].className === 'active'){
+                    buttons[i].className === '';
+                    break;
+                }
+            }
+            log(buttons[index-1]);
+            buttons[index-1].className = 'active';
+        }
+
         function animate(offset){
             if(offset === 0){
                 return;
@@ -33,6 +45,7 @@
                 list.style.left = currLeft + 'px';
                 if(currLeft > 0){
                     list.style.left = -2400 + 'px';
+
                 }
                 if(currLeft < -2400){
                     list.style.left = 0 + 'px';
@@ -41,9 +54,13 @@
             go();
         }
         bindEvent(prev,'click',function(){
+            index -= 1;
+            // showDot();
             animate(-600);
         });
         bindEvent(next,'click',function(){
+            index += 1;
+            // showDot();
             animate(600);
         });
         
